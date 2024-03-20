25d1acaf78255af101d78f8fdbd38d2d284a5f0b
Michael Frontz <24865501+mfrontz@users.noreply.github.com>
2024-03-19 21:37:40 -0400

    Upload lab9 data

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from utils.b2 import B2
 # ------------------------------------------------------
 #                      APP CONSTANTS
 # ------------------------------------------------------
-#REMOTE_DATA = 'data_file.csv'
+REMOTE_DATA = 'vaccination_data_lab9.csv'
 
 # ------------------------------------------------------
 #                        CONFIG
@@ -16,21 +16,25 @@ from utils.b2 import B2
 load_dotenv()
 
 # load Backblaze connection
-#b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
-#        key_id=os.environ['B2_KEYID'],
-#       secret_key=os.environ['B2_APPKEY'])
+b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
+        key_id=os.environ['B2_KEYID'],
+       secret_key=os.environ['B2_APPKEY'])
 
 # ------------------------------------------------------
 #                        CACHING
 # ------------------------------------------------------
 #@st.cache_data
-#def get_data():
-#    b2.set_bucket(os.environ['B2_BUCKETNAME'])
-#    df_data = b2.get_df(REMOTE_DATA)
-#    return df_data
+def get_data():
+    b2.set_bucket(os.environ['B2_BUCKETNAME'])
+    df_data = b2.get_df(REMOTE_DATA)
+    return df_data
 
 # ------------------------------------------------------
 #                         APP
 # ------------------------------------------------------
 
 st.title('Vaccinations Among Young Children')
+
+df_vacc = get_data()
+
+st.dataframe(df_vacc)
