db13d79f9a17efe7b203f3a57adcced0bebf2213
Michael Frontz <24865501+mfrontz@users.noreply.github.com>
2024-03-19 21:49:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-#import os
+import os
 
 import pandas as pd
 import numpy as np
 
 import streamlit as st
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
+
+from utils.b2 import B2
 
 # ------------------------------------------------------
 #                      APP CONSTANTS
 # ------------------------------------------------------
-DATA_FILE = 'vaccination_data_lab9.csv'
+REMOTE_DATA = 'vaccination_data_lab9.csv'
+
+# ------------------------------------------------------
+#                        CONFIG
+# ------------------------------------------------------
+load_dotenv()
+
+# load Backblaze connection
+b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
+        key_id=os.environ['B2_KEYID'],
+        secret_key=os.environ['B2_APPKEY'])
+
+# ------------------------------------------------------
+#                        CACHING
+# ------------------------------------------------------
+@st.cache_data
+def get_data():
+    b2.set_bucket(os.environ['B2_BUCKETNAME'])
+    df_remote = b2.get_df(REMOTE_DATA)
+    return df_remote
 
 # ------------------------------------------------------
 #                         APP
@@ -17,6 +38,7 @@ DATA_FILE = 'vaccination_data_lab9.csv'
 
 st.title('Vaccinations Among Young Children')
 
-df_vacc = pd.read_csv(DATA_FILE)
+#df_vacc = pd.read_csv(REMOTE_DATA)
+df_vacc = get_data()
 
 st.dataframe(df_vacc)
